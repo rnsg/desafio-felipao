cf050075957d80457f5930c4732a79716214a0ce
Rebecca <120972472+rnsg@users.noreply.github.com>
2023-10-27 06:49:44 -0300

    Update desafioDoFelipao.js

--- desafioDoFelipao.js
+++ desafioDoFelipao.js
@@ -3,9 +3,40 @@
 //Objetivo: crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
 
 let nome = "felipao"
-let XP = 8.900
+let XP = 2.000
 
+switch (XP){case 0 < 1.000:
+    console.log("O Herói "+nome," está no nível Ferro")
+    break
 
+    case 1.001 > 2.000:
+    console.log("O Herói "+nome," está no nível Bronze")
+    break
+
+    case 2.001 > 5.000:
+    console.log("O Herói "+nome," está no nível Prata")
+    break
+
+    case 5.001 > 7.000:
+    console.log("O Herói "+nome, " está no nível Ouro")
+    break
+
+    case 7.001 > 8.000:
+    console.log("O Herói "+nome, " está no nível Platina")
+    break
+
+    case 8.001 > 9.000:
+    console.log("O Herói "+nome, " está no nível Ascendente")
+    break
+
+    case 9.001 > 10.000:
+    console.log("O Herói "+nome, " está no nível Imortal")
+    break
+
+    default:
+    console.log("O Herói "+nome, " está no nível Radiante")
+    break
+}
 
 //Se XP for menor do que 1.000 = Ferro
 //Se XP for entre 1.001 e 2.000 = Bronze
